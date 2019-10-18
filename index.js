78364f056197403eff2ea09e17fa6c597a725314
spencerwright1993 <spencer.wright1993@gmail.com>
2019-10-17 21:31:08 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function add (a, b) {
+    const result= a+b;
+    return result;
+}
+console.log(add(15, 3));
+function subtract (a, b) {
+    const result= a-b;
+    return result;
+}
+console.log(subtract(22, 12));
+function multiply (e, f){
+    const result= e*f;
+    return result; 
+}
+console.log (multiply(5, 5));
+function divide (g, h){
+    const result= g/h;
+    return result;
+}
+    console.log(divide(12, 4));
+
